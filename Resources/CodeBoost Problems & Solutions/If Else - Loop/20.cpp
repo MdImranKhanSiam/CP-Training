f8a503ca77b4a45530ea643d8acc4ec4ac52eade
Imran Khan <mdimrankhansiam@gmail.com>
2026-08-09 17:45:58 +0600

    4.7

---
+++ Resources/CodeBoost Problems & Solutions/If Else - Loop/20.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+
+int main()
+{
+    int a;
+
+    cin >> a;
+
+    if (a % 2 == 0) {
+
+        cout << "Even" << endl;
+
+    } else {
+
+        cout << "Odd" << endl;
+    }
+}
---
+++ Resources/CodeBoost Problems & Solutions/If Else - Loop/21.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+
+int main()
+{
+    int a;
+
+    cin >> a;
+
+    if (a > 0) {
+
+        cout << "Positive" << endl;
+
+    } else if (a < 0) {
+
+        cout << "Negative" << endl;
+
+    } else {
+
+        cout << "Zero" << endl;
+    }
+}
